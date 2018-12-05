549cce9e38332aec51f520e46d6fad9ffd406c46
Mohammad Fidaali <mohammad.fidaali@gmail.com>
2018-12-05 11:36:41 -0800

    Minor modifications to debounce circuit

--- async_debounce_N.sv
+++ async_debounce_N.sv
@@ -8,19 +8,18 @@ output logic sync_out
 
 );
 
-logic async_in_ff;
-logic async_in_ff2;
-logic sync_out_reg, sync_out_next;
+logic async_in_ff, async_in_ff2; //Flop 2 clocks of inputs to find out if input levels are changing
+logic sync_out_reg, sync_out_next; //Register to store output value
+logic clear_counter;
 logic [LOG2(N)+1:0] count;
 
-//Flop 2 clocks of inputs to find out if input levels are changing
 always_ff @(posedge clock or posedge reset)
 begin
   if (reset) begin
     async_in_ff <=0; 
     async_in_ff2<=0;
     sync_out_reg<=0;
-	 end
+  end
   else begin 
     async_in_ff  <= async_in;
     async_in_ff2 <= async_in_ff;
@@ -28,7 +27,8 @@ begin
   end
 end
 
-assign clear= async_in_ff ^ async_in_ff2; //Use clear to reset counter because input has changed
+//clear counter because input has changed or count has reached max value
+assign clear_counter= (async_in_ff ^ async_in_ff2) | (count==N); 	
 
 //start counter if input is stable
 always_ff @(posedge clock or posedge reset)
@@ -37,13 +37,11 @@ begin
     count<= 0;
   else if (clear)
     count<= 0;
-  else if (count==N)
-	 count<= 0;
   else       
-    count<= count+1;
+    count<= count+1'b1;
 end 
 
-assign thresh = (count>=N-1); //Use N-1 as threshold since one clock has already progressed from 1st flop
+assign thresh = (count==N-1); //Use N-1 as threshold since one clock has already progressed from 1st flop
 assign sync_out_next = thresh ? async_in_ff2 : sync_out_reg; // Take new input value if count is reached, or else keep old output value
 assign sync_out = sync_out_reg;
 
